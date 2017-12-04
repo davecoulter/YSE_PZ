af2747bc5efaeaf45b2cab41507fdd601361b7ac
davecoulter <davecoulter@gmail.com>
2017-12-04 13:20:11 -0800

    Commenting out some settings

--- YSE_PZ/settings.py
+++ YSE_PZ/settings.py
@@ -100,10 +100,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-       'rest_framework.authentication.TokenAuthentication',
-       'rest_framework.authentication.SessionAuthentication',
-    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #    'rest_framework.authentication.TokenAuthentication',
+    #    'rest_framework.authentication.SessionAuthentication',
+    # ],
     'PAGE_SIZE': 10
 }
 
