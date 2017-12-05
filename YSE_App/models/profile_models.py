5866f3c107a0aa841921c95165564829fde0c400
davecoulter <davecoulter@gmail.com>
2017-12-04 22:58:10 -0800

    Adding str rep for profile

--- YSE_App/models/profile_models.py
+++ YSE_App/models/profile_models.py
@@ -14,3 +14,8 @@ class Profile(BaseModel):
 	phone_first_three = models.IntegerField()
 	phone_last_four = models.IntegerField()
 	phone_provider_str = models.CharField(max_length=64)
+
+	def __str__(self):
+		return "User: %s; Phone: +%s %s-%s-%s; Provider: %s" % (self.user.username, 
+			self.phone_country_code, self.phone_area, self.phone_first_three,
+			self.phone_last_four, self.phone_provider_str)
