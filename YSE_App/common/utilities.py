686e2455889272efaf9f131731dc34a1e13ad272
David Jones <David@Davids-MacBook-Pro-3.local>
2017-12-13 22:32:25 -0800

    sexagesimal bug fixed

--- YSE_App/common/utilities.py
+++ YSE_App/common/utilities.py
@@ -17,12 +17,15 @@ def GetSexigesimalString(ra_decimal, dec_decimal):
 	ra = c.ra.hms
 	dec = c.dec.dms
 
-	ra_string = "%02d:%02d:%0.1f" % (ra[0],ra[1],ra[2])
-	dec_string = "%02d:%02d:%0.1f" % (dec[0],np.abs(dec[1]),np.abs(dec[2]))
-
+	ra_string = "%02d:%02d:%04.1f" % (ra[0],ra[1],ra[2])
+	if dec[0] >= 0:
+		dec_string = "%02d:%02d:%04.1f" % (dec[0],np.abs(dec[1]),np.abs(dec[2]))
+	else:
+		dec_string = "%03d:%02d:%04.1f" % (dec[0],np.abs(dec[1]),np.abs(dec[2]))
+		
 	# Python has a -0.0 object. If the deg is this (because object lies < 60 min south), the string formatter will drop the negative sign
 	if c.dec < 0.0 and dec[0] == 0.0:
-		dec_string = "-0:%02d:%0.1f" % (np.abs(dec[1]),np.abs(dec[2]))
+		dec_string = "-00:%02d:%04.1f" % (np.abs(dec[1]),np.abs(dec[2]))
 
 	return (ra_string, dec_string)
 
