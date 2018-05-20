84c320ebf796d91779535ac2aafca0f525d0ea64
David Jones <David@Davids-MacBook-Pro-6.local>
2018-05-20 16:17:29 -0700

    wsgi fix for apache servers

--- YSE_PZ/wsgi.py
+++ YSE_PZ/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
+import sys
+sys.stdout = sys.__stdout__
+sys.stderr = sys.__stderr__
+
 import os
 
 from django.core.wsgi import get_wsgi_application
