272bf671c8f3a578743750f3c859a55cf0ae0df1
David Jones <David@Davids-MacBook-Pro-6.local>
2018-05-20 16:13:04 -0700

    sick of wget

--- YSE_App/util/mkFinderChart.py
+++ YSE_App/util/mkFinderChart.py
@@ -13,7 +13,7 @@ import numpy as np
 from photutils import CircularAperture,aperture_photometry
 import pylab as plt
 import time
-import wget
+import urllib
 
 class finder():
 	def __init__(self):
@@ -285,7 +285,7 @@ def panstamps_lite(ra,dec,filt,size,outfile):
 			outdlfile = '%.7f_%.7f_%s.PS1.fits'%(ra,dec,time.time())
 		else:
 			outdlfile = '%s/%.7f_%.7f_%s.PS1.fits'%(os.path.dirname(outfile),ra,dec,time.time())
-		dlfile = wget.download(s,out=outdlfile)
+		urllib.urlretrieve (s, filename=outdlfile)
 		break
 
 	if os.path.exists(dlfile):
@@ -299,7 +299,7 @@ def getDSSImage(ra,dec,filt,size,outfile):
 	else:
 		outdlfile = '%s/%.7f_%.7f_%s.DSS.fits'%(os.path.dirname(outfile),ra,dec,time.time())
 
-	dlfile = wget.download(QueryUrl,out=outdlfile)
+	dlfile = urllib.urlretrieve(QueryUrl,filename=outdlfile)
 	
 	if os.path.exists(dlfile):
 		return(dlfile)
