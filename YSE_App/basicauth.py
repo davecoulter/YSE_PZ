886baa7da5ad197160f5032ec98e47f67015346d
David Jones <david@turanga.ucsc.edu>
2019-12-18 09:18:11 -0800

    serializer missing a field

--- YSE_App/basicauth.py
+++ YSE_App/basicauth.py
@@ -11,7 +11,7 @@ from django.contrib.auth import authenticate, login
 import base64
 def login_or_basic_auth_required(view):
 	def _decorator(request, *args, **kwargs):
-		if 'HTTP_AUTHORIZATION' in request.META.keys():
+		if 'META' in request.__dict__.keys() and 'HTTP_AUTHORIZATION' in request.META.keys():
 			auth_method, credentials = request.META['HTTP_AUTHORIZATION'].split(' ', 1)
 			if auth_method.lower() == 'basic':
 				credentials = base64.b64decode(credentials.strip()).decode('utf-8')
@@ -26,8 +26,25 @@ def login_or_basic_auth_required(view):
 			response.status_code = 401
 			response['WWW-Authenticate'] = 'Basic'
 			return response
+		elif 'request' in request.__dict__.keys() and 'HTTP_AUTHORIZATION' in request.request.META.keys():
+			auth_method, credentials = request.request.META['HTTP_AUTHORIZATION'].split(' ', 1)
+			if auth_method.lower() == 'basic':
+				credentials = base64.b64decode(credentials.strip()).decode('utf-8')
+				username, password = credentials.split(':', 1)
+				user = auth.authenticate(username=username, password=password)
+				if user is not None and user.is_active:
+					# Correct password, and the user is marked "active"
+					return view(request, *args, **kwargs)
+				else:
+					return HttpResponseForbidden('Incorrect user credentials.')
+			response = HttpResponse()
+			response.status_code = 401
+			response['WWW-Authenticate'] = 'Basic'
+			return response			
 		else:
-			if request.user.is_authenticated:
+			if 'user' in request.__dict__.keys() and request.user.is_authenticated:
+				return view(request, *args, **kwargs)
+			elif 'request' in request.__dict__.keys() and request.request.user.is_authenticated:
 				return view(request, *args, **kwargs)
 			else:
 				return HttpResponseForbidden('Incorrect user credentials')
