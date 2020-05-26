40056fe06f61e4c298c67002df1ca0f100e1ede7
djones1040 <djones@pha.jhu.edu>
2020-05-26 12:23:47 -0700

    disabled yellow color.....

--- YSE_App/view_utils.py
+++ YSE_App/view_utils.py
@@ -667,7 +667,7 @@ def lightcurveplot_summary(request, transient_id, salt2=False):
 	#ax.title.text = "%s"%transient.name
 	#colorlist = ['#1f77b4','#ff7f0e','#2ca02c','#d62728',
 	#			 '#9467bd','#8c564b','#e377c2','#7f7f7f','#bcbd22','#17becf']
-	colorlist = ['#8dd3c7','#ffffb3','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9']
+	colorlist = ['#8dd3c7','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5','#d9d9d9']
 	count = 0
 	allband = np.append(band,upperlimband)
 	allbandstr = np.append(bandstr,upperlimbandstr)
