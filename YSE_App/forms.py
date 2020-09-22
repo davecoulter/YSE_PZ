b01d5011bf058a80de5bbc6d4a7f00d284de3d7a
djones1040 <djones@pha.jhu.edu>
2020-09-22 14:52:51 -0700

    added APO/DIS to spec upload instruments list

--- YSE_App/forms.py
+++ YSE_App/forms.py
@@ -252,7 +252,7 @@ class SpectrumUploadForm(ModelForm):
 		'OSIRIS','FLOYDS-N','FLOYDS-S','NIRC2',
 		'NIRSPEC','NIRES','KCWI','ESI',
 		'DEIMOS','OSIRIS','MOSFIRE','LRIS',
-		'HIRES','GMOS','Goodman','KAST','WiFeS','WFCCD']
+		'HIRES','GMOS','Goodman','KAST','WiFeS','WFCCD','DIS']
 	instrument = forms.ModelChoiceField(Instrument.objects.filter(Q(name__in=spec_instruments)))
 	#import pdb; pdb.set_trace()
 	class Meta:
