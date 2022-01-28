3e07cd262a7b5e3c34a85c7b02a6f24be7f8ffd7
Peter <18149966+astrophpeter@users.noreply.github.com>
2022-01-28 12:08:43 -0800

    added docstring to base model.

--- YSE_App/models/base.py
+++ YSE_App/models/base.py
@@ -3,7 +3,16 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class BaseModel(models.Model):
-
+	"""
+	Base model to track and audit changes to data models in YSE_PZ.
+	
+	
+	Attributes:
+		created_by (models.ForeignKey): YSE_PZ User that created the data model.
+		created_date (model.DateTimeField): Date the YSE_PZ User created the data model.
+		modified_by (models.ForeignKey): The last YSE_PZ User to modify the data model.
+		modified_date (models.DateTimeField): The last time a YSE_PZ User mofified the data model.
+	"""
 	class Meta:
 		abstract = True
 
