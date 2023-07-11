23b6098648a6761dbf6c4f2517942feb86a7a4f6
profxj <xavier@ucolick.org>
2023-07-10 18:12:11 -0700

    update table

--- YSE_App/scripts/add_chime_test.py
+++ YSE_App/scripts/add_chime_test.py
@@ -4,6 +4,7 @@ from pkg_resources import resource_filename
 from YSE_App.models import Transient
 
 from django.contrib import auth
+from django.db.models import ForeignKey
 
 import pandas
 
@@ -11,7 +12,7 @@ import pandas
 def run():
     # Load up the table
     csv_file = os.path.join(
-        resource_filename('YSE_App', 'tests'), 'chime_tests.csv')
+        resource_filename('YSE_App', 'test_files'), 'chime_tests.csv')
     df_frbs = pandas.read_csv(csv_file)
 
     user = auth.authenticate(username='root', password='F4isthebest')
@@ -20,7 +21,18 @@ def run():
     for ss in range(len(df_frbs)):
 
         transient = df_frbs.iloc[ss]
+        transientkeys = transient.keys()
+
         transientdict = {'created_by_id':user.id,
                          'modified_by_id':user.id}
 
         for transientkey in transientkeys:
+            if transientkey == 'transientphotometry' or \
+                transientkey == 'transientspectra' or \
+                transientkey == 'host' or \
+                transientkey == 'tags' or \
+                transientkey == 'gw' or \
+                transientkey == 'non_detect_instrument' or \
+                transientkey == 'internal_names': continue
+            if not isinstance(Transient._meta.get_field(transientkey), ForeignKey):
+                if transient[transientkey] is not None: transientdict[transientkey] = transient[transientkey]
