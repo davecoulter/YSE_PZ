d1f70ccd492d2d771280f4e0bb896404a81802d1
profxj <xavier@ucolick.org>
2023-07-11 10:38:16 -0700

    another tab

--- YSE_App/chime/tags.py
+++ YSE_App/chime/tags.py
@@ -1,21 +1,21 @@
-""" Specify CHIME Surveys """
+""" Specify CHIME Tags """
 
 blind_survey = dict(
+    name='CHIME-Blind',
     ra_rngs=[[0, 225]],
     dec_rngs=[[-10, 10]],
     label=['Stripe 82'],
     )
 
 def set_from_instance(instance):
+    # Blind?
+    tags = []
+    for ra_rng, dec_rng, label in zip(
+        blind_survey['ra_rngs'], 
+        blind_survey['dec_rngs'], 
+        blind_survey['label']):
+        if instance.ra > ra_rng[0] and instance.ra < ra_rng[1] and \
+            instance.dec > dec_rng[0] and instance.dec < dec_rng[1]:
+            tags.append(blind_survey['name'])
 
-   # Blind?
-   surveys = []
-   for ra_rng, dec_rng, label in zip(
-       blind_survey['ra_rngs'], 
-       blind_survey['dec_rngs'], 
-       blind_survey['label']):
-       if instance.ra > ra_rng[0] and instance.ra < ra_rng[1] and \
-           instance.dec > dec_rng[0] and instance.dec < dec_rng[1]:
-           surveys.append('Blind')
-
-    return surveys
+    return tags
