e27397338b5af37821556090885c26e74b08fc9c
J. Xavier Prochaska <xavier@ucolick.org>
2023-10-09 12:22:15 -0700

    deccal

--- YSE_App/frb_init.py
+++ YSE_App/frb_init.py
@@ -1,11 +1,29 @@
 """ Methods to init the DB """
 
 from YSE_App.models import TransientStatus
+from YSE_App.models import ObservationGroup
 from YSE_App.frb_utils import add_or_grab_obj
 
 from YSE_App import frb_status
 
+def init_obsgroups(user):
+    """ Initialize the transient status table
+
+    Args:
+        user (): user
+    """
+
+    # Add into DB
+    for obsgroup in ['DECaLS']:
+        _ = add_or_grab_obj(ObservationGroup,
+                        dict(name=obsgroup), {}, user)
+
 def init_statuses(user):
+    """ Initialize the transient status table
+
+    Args:
+        user (): user
+    """
 
     # Delete all existing
     TransientStatus.objects.all().delete()
