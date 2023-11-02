0f978c9e21504c25222b1aadd056bbb3575644bd
profxj <xavier@ucolick.org>
2023-11-02 02:59:58 -0700

    ready

--- YSE_App/chime/chime_kko.py
+++ YSE_App/chime/chime_kko.py
@@ -36,31 +36,16 @@ def init_kko(clean:bool=True):
 
     return
 
-    # Load up the table
-    csv_file = os.path.join(
-        resource_filename('YSE_App', 'chime'), 'chime_kko_sep2023.csv')
-    df_frbs = pandas.read_csv(csv_file)
 
-    # Add CHIME Observation Group?
-    survey_names = [survey.name for survey in FRBSurvey.objects.all()]
-    for survey in ['CHIME/FRB']:
-        if survey not in survey_names:
-            obs = FRBSurvey(name=survey, created_by_id=user.id, modified_by_id=user.id)
-            obs.save()
+def init_DMISM():
 
-    # Add em 
-    transients = ctu.add_df_to_db(df_frbs, user, 
-                         delete_existing=False)
+    for frb in FRBTransient.objects.all():
+        if frb.DM_ISM is None:
+            print(f'Working on: {frb.name}')
+            # 
+            frb.DM_ISM = frb.calc_DM_ISM()
+            # Set status
+            frb_status.set_status(frb)
 
-    # Set the KKO tag
-    kko_tag = frb_utils.add_or_grab_obj(
-            FRBTag, dict(name='CHIME-KKO'), {}, user)
-    unkn_tag = frb_utils.add_or_grab_obj(
-            FRBTag, dict(name='CHIME-Unknown'), {}, user)
-    for transient in transients:
-        # Drop unknown
-        transient.frb_tags.remove(unkn_tag)
-        #
-        transient.frb_tags.add(kko_tag)
-        # Update status (and save)
-        frb_status.set_status(transient)
+            # Save
+            frb.save()
